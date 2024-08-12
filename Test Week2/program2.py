1aef42bc6fc47ab700cd6817ab819bcba4cebca7
Devika Ashok <164758771+DevikaAshok12@users.noreply.github.com>
2024-08-12 09:57:42 +0530

    Update program2.py

--- Test Week2/program2.py
+++ Test Week2/program2.py
@@ -3,16 +3,22 @@ class Eatable:
         self.carbs = carbs
         self.fat = fat
         self.protein = protein
+        def describe(self):
+        return f"Carbs: {self.carbs}g, Fat: {self.fat}g, Protein: {self.protein}g"
+
+        
        
 class Vegetarian(Eatable):
     def __init__(self, carbs, fat, protein, isPeelable):
         super().__init__(carbs, fat, protein)
         self.isPeelable = isPeelable
+        def describe(self):
 
 class NonVegetarian(Eatable):
     def __init__(self, carbs, fat, protein, isBoneless):
         super().__init__(carbs, fat, protein)
         self.isBoneless = isBoneless
+        def describe(self):
         
 food_item_dict = {1: 'Vegetarian', 2: 'NonVegetarian'}
 peelable_dict = {1: True, 2: False}
